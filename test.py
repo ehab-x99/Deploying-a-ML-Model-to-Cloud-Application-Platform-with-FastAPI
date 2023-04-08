614d99c748dce8bc3445b4664f13ee45507a4666
ehab-x99 <121483346+ehab-x99@users.noreply.github.com>
2023-04-08 18:12:35 +0300

    update test_api and Unit_test

--- test.py
+++ test.py
@@ -5,7 +5,63 @@ import sklearn
 from sklearn.model_selection import train_test_split
 from starter.ml.data import process_data
 from starter.ml.model import train_model, compute_model_metrics, inference
+import json
+from fastapi.testclient import TestClient
+from app.main import app
+client = TestClient(app)
 
+
+# A function to test the get
+def test_get():
+    response = client.get("/welcome")
+    assert response.status_code == 200
+    assert response.json() == {"Hello, welcome to our app!"}
+
+
+# A function to test the post on a predicted value of Salary <50K
+def test_post_1():
+    input_dict = {
+        "age": 25,
+        "workclass": "Self-emp-not-inc",
+        "fnlgt": 176756,
+        "education": "HS-grad",
+        "education-num": 9,
+        "marital-status": "Never-married",
+        "occupation": "Farming-fishing",
+        "relationship": "Own-child",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 35,
+        "native-country": "United-States"
+    }
+    response = client.post("/predict", json=input_dict)
+    assert response.status_code == 200
+    assert response.json() == {'prediction': 'Income <= 50K'}
+
+
+# A function to test the post on a predicted value of Salary>50K
+def test_post_2():
+    input_dict = {
+        "age": 57,
+        "workclass": "Federal-gov",
+        "fnlgt": 337895,
+        "education": "Bachelors",
+        "education-num": 13,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Prof-specialty",
+        "relationship": "Husband",
+        "race": "Black",
+        "sex": "Female",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States"
+    }
+    response = client.post("/predict", json=input_dict)
+    assert response.status_code == 200
+    assert response.json() == {'prediction': 'Income > 50K'}
 # load the data.
 data = pd.read_csv(os.path.join(os.getcwd(), "project-3/data/census.csv"))
 
@@ -61,3 +117,8 @@ if __name__ == "__main__":
     test_train_model()
     test_compute_model_metrics()
     test_inference()
+
+    test_get()
+    test_post_1()
+    test_post_2()
+
